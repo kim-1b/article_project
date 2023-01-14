1998360b3f86f21464909a957f8bbdfe31613c2c
kim-1b <105867788+kim-1b@users.noreply.github.com>
2023-01-14 14:43:30 +0900

    Update Notice.js

--- src/pages/Notice.js
+++ src/pages/Notice.js
@@ -18,7 +18,7 @@ function Notice() {
 
   const 게시글정보가져 = async () => {
     await axios({
-      url: "http://18.183.82.9:22/notice",
+      url: "http://18.183.82.9:3306/notice",
       method: "GET",
     }).then((response) => {
       setNotice(response.data);
