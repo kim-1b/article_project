29e83a5c82a87231c45f48f2c084d700641e9f7a
kim-1b <105867788+kim-1b@users.noreply.github.com>
2023-01-14 14:43:14 +0900

    Update Join.js

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -18,7 +18,7 @@ function Join() {
 
   const 가입하기 = async () => {
     await axios({
-      url: "http://18.183.82.9:22/join",
+      url: "http://18.183.82.9:3306/join",
       method: "POST",
       data: data,
     }).then((response) => {
